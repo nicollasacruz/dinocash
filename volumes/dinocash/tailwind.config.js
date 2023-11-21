a7b60b6b52987dec444b54e4805dcc7033b079b7
Nicollas Cruz <nicollasacruz@gmail.com>
2023-11-21 16:37:25 -0300

    adicionado tratativa de tamanho de tela

--- volumes/dinocash/tailwind.config.js
+++ volumes/dinocash/tailwind.config.js
@@ -29,6 +29,18 @@ export default {
                 ]
             }
         },
+        screens: {
+            'mobile': '300px',
+            // => @media (min-width: 300px) { ... }
+            'tablet': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'laptop': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'desktop': '1280px',
+            // => @media (min-width: 1280px) { ... }
+        },
     },
 
     plugins: [forms],
