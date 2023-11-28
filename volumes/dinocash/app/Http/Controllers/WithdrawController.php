995129486e9bc4307217839e96c44e87fce9e47d
Nicollas Cruz <nicollasacruz@gmail.com>
2023-11-27 23:00:38 -0300

    withdrawTotal

--- volumes/dinocash/app/Http/Controllers/WithdrawController.php
+++ volumes/dinocash/app/Http/Controllers/WithdrawController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Deposit;
+use App\Models\User;
 use App\Models\Withdraw;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
@@ -16,10 +18,14 @@ class WithdrawController extends Controller
     {
         $withdraws = Withdraw::with('user')->get();
         $totalToday = Withdraw::whereDate('created_at', Carbon::today())->where('type', 'paid')->sum('amount');
-        
+        $withdrawsAmount = Withdraw::where('type', 'paid')->sum('amount');
+        $depositsAmount = Deposit::where('type', 'paid')->sum('amount');
+        $walletsAmount = User::where('role','user')->sum('wallet');
+        $totalAmount = ($depositsAmount - $withdrawsAmount - $walletsAmount) / 100;
         return Inertia::render('Requests', [
             'withdraws' => $withdraws,
-            'totalToday'=> $totalToday
+            'totalToday'=> $totalToday,
+            'totalAmount' => $totalAmount,
         ]);
     }
 }
