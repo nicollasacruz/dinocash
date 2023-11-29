bb3173c761d4274099ff34d394f094bdd4f564bd
Nicollas Cruz <nicollasacruz@gmail.com>
2023-11-29 00:46:47 -0300

    seeder completa

--- volumes/dinocash/database/seeders/DatabaseSeeder.php
+++ volumes/dinocash/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,46 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\AffiliateWithdraw;
+use App\Models\Deposit;
+use App\Models\Withdraw;
 use Illuminate\Database\Seeder;
+use App\Models\GameHistory;
+use Faker\Factory as Faker;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $faker = Faker::create();
+        foreach (range(1, 10) as $index) {
+            User::factory()->withInvitationLink((string) $faker->randomNumber(4))->create();
+            Deposit::factory()->create();
+            Deposit::factory()->paid()->create();
+            Withdraw::factory()->create();
+            Withdraw::factory()->paid()->create();
+            Withdraw::factory()->rejected()->create();
+            AffiliateWithdraw::factory()->create();
+            AffiliateWithdraw::factory()->paid()->create();
+            AffiliateWithdraw::factory()->rejected()->create();
+        }
+        foreach (range(1, 100) as $index) {
+            $user = User::factory()->create();
+            $affiliateId = $faker->randomNumber(1);
+            $user->addReferral($faker->randomElement(User::where('isAffiliate', true)->get()));
+            $user->save();
+            $type = $faker->randomElement(['win', 'loss']);
+            $amount = $faker->randomNumber(3) * ($type === 'win' ? 1 : -1);
+            GameHistory::create([
+                'Amount' => $amount,
+                'finalAmount' => $amount < 0 ? $amount : $amount * 2 ,
+                'userId' => $user->id,
+                'type' => $type,
+            ]);
+        }
     }
 }
