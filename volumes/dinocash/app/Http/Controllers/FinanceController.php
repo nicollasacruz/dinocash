1383409acbe6dad888dbf071fe9f2e63e254fc2e
Nicollas Cruz <nicollasacruz@gmail.com>
2023-11-29 16:55:43 -0300

    ajuste finance

--- volumes/dinocash/app/Http/Controllers/FinanceController.php
+++ volumes/dinocash/app/Http/Controllers/FinanceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AffiliateHistory;
+use App\Models\AffiliateWithdraw;
 use App\Models\Deposit;
 use App\Models\GameHistory;
 use App\Models\User;
@@ -42,6 +43,17 @@ class FinanceController extends Controller
                 }
             ])
             ->sum('amount');
+        $withdrawsAmountAffiliate = AffiliateWithdraw::when($dateStart && $dateEnd, function ($query) use ($dateStart, $dateEnd) {
+            $query->whereBetween('updated_at', [$dateStart, $dateEnd]);
+        })
+            ->where('type', 'paid')
+            ->with([
+                'user' => function ($query) {
+                    $query
+                        ->where('isAffiliate', false);
+                }
+            ])
+            ->sum('amount');
         $totalReceived = GameHistory::when($dateStart && $dateEnd, function ($query) use ($dateStart, $dateEnd) {
             $query->whereBetween('updated_at', [$dateStart, $dateEnd]);
         })
@@ -71,7 +83,13 @@ class FinanceController extends Controller
             ->select('users.email', 'withdraws.amount')
             ->orderByDesc('amount')
             ->take(3)
-            ->get();
+            ->get()
+            ->map(function ($withdraw) {
+                return [
+                    'amount' => $withdraw->amount / 100,
+                    'user_email' => $withdraw->email,
+                ];
+            });
 
         $topDeposits = Deposit::where('type', 'paid')
             ->where('deposits.updated_at', '>=', now()->subDay())
@@ -79,7 +97,13 @@ class FinanceController extends Controller
             ->select('users.email', 'deposits.amount')
             ->orderByDesc('amount')
             ->take(3)
-            ->get();
+            ->get()
+            ->map(function ($deposit) {
+                return [
+                    'amount' => $deposit->amount / 100,
+                    'user_email' => $deposit->email,
+                ];
+            });
 
         $topProfitableAffiliates = $referralService->getTopReferralsByProfit();
         $topLossAffiliates = $referralService->getTopReferralsByLoss();
@@ -91,11 +115,11 @@ class FinanceController extends Controller
         $totalAmount = ($depositsAmountCaixa - $withdrawsAmountCaixa - $walletsAmountCaixa - $walletsAfilliateAmountCaixa);
 
         return Inertia::render("Admin/Finances", [
-            'totalAmount' => $totalAmount,
-            'depositsAmount' => $depositsAmount,
-            'withdrawsAmount' => $withdrawsAmount,
-            'totalReceived' => $totalReceived,
-            'totalPaid' => $totalPaid,
+            'totalAmount' => $totalAmount / 100,
+            'depositsAmount' => $depositsAmount / 100,
+            'withdrawsAmount' => $withdrawsAmount / 100 + $withdrawsAmountAffiliate / 100,
+            'totalReceived' => ($totalReceived * -1) / 100,
+            'totalPaid' => $totalPaid / 100,
             'topWithdraws' => $topWithdraws,
             'topDeposits' => $topDeposits,
             'topProfitableAffiliates' => $topProfitableAffiliates,
