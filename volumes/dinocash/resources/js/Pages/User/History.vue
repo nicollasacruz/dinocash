9e349f05774e53bcbcdf7879c2fa9d99c5cca002
Nicollas Cruz <nicollasacruz@gmail.com>
2023-12-05 23:59:31 -0300

    linkado historico e movimentacao do user

--- volumes/dinocash/resources/js/Pages/User/History.vue
+++ volumes/dinocash/resources/js/Pages/User/History.vue
@@ -1,89 +1,47 @@
 <template>
-    <UserLayouyt>
-        <div class="p-2 lg:px-8">
-            <div class="text-center uppercase text-3xl text-gray-800 mb-4">
-                Histórico
-            </div>
-            <div class="w-full flex-col flex gap-y-4 text-gray-800">
-                <div class="grid grid-cols-4 text-[11px] lg:text-xl text-center">
-                    <div>Data</div>
-                    <div>
-                        Distancia
-                    </div>
-                    <div>
-                        Resultado
-                    </div>
-                    <div>Valor</div>
-                </div>
-                <div v-for="{date, distance, result, amount},i in data" class="grid grid-cols-4 bg-gray-200 py-2 text-sm lg:text-xl text-center">
-                    <div>
-                        {{ dayjs(date).format("DD/MM/YYYY") }}
-                    </div>
-                    <div>
-                        {{ distance }}
-                    </div>
-                    <div>
-                        {{ result }}
-                    </div>
-                    <div>
-                        {{ toBRL(amount) }}
-                    </div>
-                </div>
-            </div>
+  <UserLayouyt>
+    <div class="p-2 lg:px-8">
+      <div class="text-center uppercase text-3xl text-gray-800 mb-4">
+        Histórico
+      </div>
+      <div class="w-full flex-col flex gap-y-4 text-gray-800">
+        <div class="grid grid-cols-4 text-[11px] lg:text-xl text-center">
+          <div>Data</div>
+          <div>Distancia</div>
+          <div>Resultado</div>
+          <div>Valor</div>
         </div>
-    </UserLayouyt>
+        <div
+          v-for="({ updated_at, distance, type, finalAmount }, i) in transactions"
+          class="grid grid-cols-4 bg-gray-200 py-2 text-sm lg:text-xl text-center"
+        >
+          <div>
+            {{ dayjs(updated_at).format("DD/MM/YYYY") }}
+          </div>
+          <div>
+            {{ distance }} M
+          </div>
+          <div :class="type === 'win' ? 'text-green-500' : 'text-red-500'">
+            {{ type === 'win' ? 'GANHOU' : 'PERDEU' }}
+          </div>
+          <div>
+            {{ toBRL(finalAmount) }}
+          </div>
+        </div>
+      </div>
+    </div>  
+  </UserLayouyt>
 </template>
 <script setup lang="ts">
 import UserLayouyt from "../..//Layouts/UserLayout.vue";
 import dayjs from "dayjs";
-const data = [
-    {
-        id: 1,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Ganhou",
-        amount: 100,
-    },
-    {
-        id: 2,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Ganhou",
-        amount: 100,
-    },
-    {
-        id: 3,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Ganhou",
-        amount: 100,
-    },
-    {
-        id: 4,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Ganhou",
-        amount: 100,
-    },
-    {
-        id: 5,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Ganhou",
-        amount: 100,
-    },
-    {
-        id: 6,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Ganhou",
-        amount: 100,
-    },
-];
+
+const { transactions } = defineProps(['transactions', ]);
+
 function toBRL(value) {
-    return new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-    }).format(value);
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
 }
 </script>
--- volumes/dinocash/resources/js/Pages/User/Movement.vue
+++ volumes/dinocash/resources/js/Pages/User/Movement.vue
@@ -1,89 +1,79 @@
 <template>
-    <UserLayouyt>
-        <div class="p-2 lg:px-8">
-            <div class="text-center uppercase text-3xl text-gray-800 mb-4">
-                Movimentação
-            </div>
-            <div class="w-full flex-col flex gap-y-4 text-gray-800">
-                <div class="grid grid-cols-4 text-[11px] lg:text-xl text-center">
-                    <div>Data</div>
-                    <div>
-                        Distancia
-                    </div>
-                    <div>
-                        Status
-                    </div>
-                    <div>Valor</div>
-                </div>
-                <div v-for="{date, distance, result, amount},i in data" class="grid grid-cols-4 bg-gray-200 py-2 text-sm lg:text-xl text-center">
-                    <div>
-                        {{ dayjs(date).format("DD/MM/YYYY") }}
-                    </div>
-                    <div>
-                        {{ distance }}
-                    </div>
-                    <div>
-                        {{ result }}
-                    </div>
-                    <div>
-                        {{ toBRL(amount) }}
-                    </div>
-                </div>
-            </div>
+  <UserLayouyt>
+    <div class="p-2 lg:px-8 h-[100%] overflow-x-auto">
+      <div class="text-center uppercase text-3xl text-gray-800 mb-4">
+        Movimentação
+      </div>
+      <div class="w-full flex-col flex gap-y-4 text-gray-800">
+        <div class="grid grid-cols-4 text-[11px] lg:text-xl text-center">
+          <div>Data</div>
+          <div>Movimentação</div>
+          <div>Status</div>
+          <div>Valor</div>
         </div>
-    </UserLayouyt>
+        <div
+          v-for="(
+            { updated_at, transaction_type, type, amount }, i 
+          ) in transactions"
+          class="grid grid-cols-4 bg-gray-200 py-2 text-sm lg:text-xl text-center"
+        >
+          <div>
+            {{ dayjs(updated_at).format("DD/MM/YYYY") }}
+          </div>
+          <div>
+            {{ transaction_type === "deposit" ? "DEPÓSITO" : "SAQUE" }}
+          </div>
+          <div :class="type === 'pending'
+                ? 'text-yellow-500'
+                : type === 'rejected'
+                ? 'text-red-500'
+                : 'text-green-500'">
+            {{
+              type === "pending"
+                ? "PENDENTE"
+                : type === "rejected"
+                ? "REJEITADO"
+                : "FINALIZADO"
+            }}
+          </div>
+          <div>
+            {{ toBRL(amount) }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </UserLayouyt>
 </template>
 <script setup lang="ts">
 import UserLayouyt from "../..//Layouts/UserLayout.vue";
 import dayjs from "dayjs";
-const data = [
-    {
-        id: 1,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Pendente",
-        amount: 100,
-    },
-    {
-        id: 2,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Pendente",
-        amount: 100,
-    },
-    {
-        id: 3,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Pendente",
-        amount: 100,
-    },
-    {
-        id: 4,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Pendente",
-        amount: 100,
-    },
-    {
-        id: 5,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Pendente",
-        amount: 100,
-    },
-    {
-        id: 6,
-        date: "2021-09-01",
-        distance: 100,
-        result: "Pendente",
-        amount: 100,
-    },
-];
+
+const { transactions } = defineProps(["transactions"]);
+
 function toBRL(value) {
-    return new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-    }).format(value);
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
 }
 </script>
+
+<style scoped>
+    .overflow-x-auto {
+        scrollbar-width: thin; /* Para navegadores que suportam a propriedade scrollbar-width */
+        scrollbar-color: #555555 #212121; /* Cor da barra de rolagem e cor do fundo da barra de rolagem */
+    }
+
+    .overflow-x-auto::-webkit-scrollbar {
+        width: 8px; /* Largura da barra de rolagem no Chrome/Safari/Edge */
+    }
+
+    .overflow-x-auto::-webkit-scrollbar-thumb {
+        background-color: #555555; /* Cor da barra de rolagem */
+        border-radius: 4px; /* Borda arredondada da barra de rolagem */
+    }
+
+    .overflow-x-auto::-webkit-scrollbar-track {
+        background-color: #212121; /* Cor do fundo da barra de rolagem */
+    }
+</style>
