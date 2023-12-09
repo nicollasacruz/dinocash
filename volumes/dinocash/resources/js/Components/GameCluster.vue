ea30a557c552fee917bb0a8f0be30d4c489dfc7d
Nicollas Cruz <nicollasacruz@gmail.com>
2023-12-09 18:05:52 -0300

    teste de orientação

--- volumes/dinocash/resources/js/Components/GameCluster.vue
+++ volumes/dinocash/resources/js/Components/GameCluster.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+  <div></div>
 </template>
 
 <script setup lang="ts">
@@ -7,85 +7,106 @@
 import DinoGame from "../lib/game/DinoGame.js";
 import { onUnmounted } from "vue";
 import { defineProps, defineEmits, watch } from "vue";
+import { onMounted } from "vue";
+
+// Função para forçar a orientação para paisagem
+const forceLandscapeOrientation = () => {
+  if (screen.orientation.lock) {
+    screen.orientation.lock("landscape").catch((err) => {
+      console.error("Erro ao forçar a orientação para paisagem:", err);
+    });
+  }
+};
+
+// Trata mudanças de orientação
+const handleOrientationChange = () => {
+  console.log("Orientação alterada:", window.orientation);
+};
+
+// Adiciona um listener quando o componente é montado
+onMounted(() => {
+  forceLandscapeOrientation();
+  window.addEventListener("orientationchange", handleOrientationChange);
+});
 const props = defineProps({
-    active: Boolean,
+  active: Boolean,
 });
 const emit = defineEmits(["endGame", "finishGame"]);
 const game = new DinoGame(700, 400);
 const isTouchDevice =
-    "ontouchstart" in window ||
-    navigator.maxTouchPoints > 0 ||
-    // @ts-ignore
-    navigator.msMaxTouchPoints > 0;
+  "ontouchstart" in window ||
+  navigator.maxTouchPoints > 0 ||
+  // @ts-ignore
+  navigator.msMaxTouchPoints > 0;
 
 // @ts-ignore
 document.addEventListener("endGame", ({ detail }) => {
-    emit("endGame", detail);
-    console.log(detail, 'endGame');
-    // destroy game instance
+  emit("endGame", detail);
+  console.log(detail, "endGame");
+  // destroy game instance
 });
 document.addEventListener("finishGame", ({ detail }) => {
-    emit("finishGame", detail);
-    console.log(detail, 'finishGame');
-    // destroy game instance
+  emit("finishGame", detail);
+  console.log(detail, "finishGame");
+  // destroy game instance
 });
 game.start().catch(console.error);
 
 const touchStartCallback = ({ touches }) => {
-    if (touches.length === 1) {
-        game.onInput("jump");
-    } else if (touches.length === 2) {
-        game.onInput("duck");
-    }
+  if (touches.length === 1) {
+    game.onInput("jump");
+  } else if (touches.length === 2) {
+    game.onInput("duck");
+  }
 };
 
 const touchEndCallback = () => {
-    game.onInput("stop-duck");
+  game.onInput("stop-duck");
 };
 
 const keydownCallback = ({ keyCode }) => {
-    const keycodes = {
-        JUMP: { 38: 1, 32: 1 },
-        DUCK: { 40: 1 },
-        FINISH: { 13: 1 },
-    };
+  const keycodes = {
+    JUMP: { 38: 1, 32: 1 },
+    DUCK: { 40: 1 },
+    FINISH: { 13: 1 },
+  };
 
-    if (keycodes.JUMP[keyCode]) {
-        game.onInput("jump");
-    } else if (keycodes.DUCK[keyCode]) {
-        game.onInput("duck");
-    } else if (keycodes.FINISH[keyCode]) {
-        game.onInput("finish");
-    }
+  if (keycodes.JUMP[keyCode]) {
+    game.onInput("jump");
+  } else if (keycodes.DUCK[keyCode]) {
+    game.onInput("duck");
+  } else if (keycodes.FINISH[keyCode]) {
+    game.onInput("finish");
+  }
 };
 
 const keyupCallback = ({ keyCode }) => {
-    const keycodes = {
-        DUCK: { 40: 1 },
-    };
+  const keycodes = {
+    DUCK: { 40: 1 },
+  };
 
-    if (keycodes.DUCK[keyCode]) {
-        game.onInput("stop-duck");
-    }
+  if (keycodes.DUCK[keyCode]) {
+    game.onInput("stop-duck");
+  }
 };
 
 watch(
-    () => props.active,
-    (newValue) => {
-        if (newValue) {
-            document.addEventListener("touchstart", touchStartCallback);
-            document.addEventListener("touchend", touchEndCallback);
-            document.addEventListener("keydown", keydownCallback);
-            document.addEventListener("keyup", keyupCallback);
-        } else {
-            document.removeEventListener("keydown", keydownCallback);
-            document.removeEventListener("keyup", keyupCallback);
-            document.removeEventListener("touchstart", touchStartCallback);
-            document.removeEventListener("touchend", touchEndCallback);
-        }
+  () => props.active,
+  (newValue) => {
+    if (newValue) {
+      document.addEventListener("touchstart", touchStartCallback);
+      document.addEventListener("touchend", touchEndCallback);
+      document.addEventListener("keydown", keydownCallback);
+      document.addEventListener("keyup", keyupCallback);
+    } else {
+      document.removeEventListener("keydown", keydownCallback);
+      document.removeEventListener("keyup", keyupCallback);
+      document.removeEventListener("touchstart", touchStartCallback);
+      document.removeEventListener("touchend", touchEndCallback);
     }
+  }
 );
 onUnmounted(() => {
-    // removeKeys();
+  // removeKeys();
 });
 </script>
