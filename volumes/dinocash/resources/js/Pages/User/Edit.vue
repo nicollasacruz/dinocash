356c058beb4cbd8c78522bd09fab6c3aa49fdcca
Nicollas Cruz <nicollasacruz@gmail.com>
2023-12-09 18:07:45 -0300

    ajuste import

--- volumes/dinocash/resources/js/Pages/User/Edit.vue
+++ volumes/dinocash/resources/js/Pages/User/Edit.vue
@@ -1,8 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import DeleteUserForm from '../Profile/Partials/DeleteUserForm.vue';
-import UpdatePasswordForm from '../Profile/Partials/UpdatePasswordForm.vue';
-import UpdateProfileInformationForm from '../Profile/Partials/UpdateProfileInformationForm.vue';
+import DeleteUserForm from '../Partials/DeleteUserForm.vue';
+import UpdatePasswordForm from '../Partials/UpdatePasswordForm.vue';
+import UpdateProfileInformationForm from '../Partials/UpdateProfileInformationForm.vue';
 import { Head } from '@inertiajs/vue3';
 
 defineProps({
