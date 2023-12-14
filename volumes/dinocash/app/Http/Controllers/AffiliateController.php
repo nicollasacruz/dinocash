5350d903f0e341c8f522a69fbd286c91d9d11b47
Nicollas Cruz <nicollasacruz@gmail.com>
2023-12-14 13:39:30 -0300

    Revert "ajuste no orderBy"
    This reverts commit 1a65c558f1612508c2b57bdde493ed23a43c75a6.

--- volumes/dinocash/app/Http/Controllers/AffiliateController.php
+++ volumes/dinocash/app/Http/Controllers/AffiliateController.php
@@ -26,7 +26,7 @@ class AffiliateController extends Controller
         try {
             $email = $request->query('email');
 
-            $affiliateWithdrawsList = AffiliateWithdraw::orderBy('created_at', 'desc')->get();
+            $affiliateWithdrawsList = AffiliateWithdraw::all()->orderBy('created_at', 'desc');
 
             $affiliates = User::when($email, function ($query) use ($email) {
                 $query->where('email', 'LIKE', '%' . $email . '%');
