55b22c2aa04462b8eb99d32750a906430407e69e
Nicollas Cruz <nicollasacruz@gmail.com>
2023-12-14 13:39:48 -0300

    Revert "teste tela de afiliados"
    This reverts commit fccd5080d5ec4215e2498aa2cca33210aa7c0207.

--- volumes/dinocash/app/Http/Controllers/AffiliateController.php
+++ volumes/dinocash/app/Http/Controllers/AffiliateController.php
@@ -26,7 +26,7 @@ class AffiliateController extends Controller
         try {
             $email = $request->query('email');
 
-            $affiliateWithdrawsList = AffiliateWithdraw::all()->orderBy('created_at', 'desc');
+            $affiliateWithdrawsList = AffiliateWithdraw::getAffiliateWithdrawLikeEmail($email);
 
             $affiliates = User::when($email, function ($query) use ($email) {
                 $query->where('email', 'LIKE', '%' . $email . '%');
