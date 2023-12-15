ba06d1cf20da8b3b767a484ef97f5ba401c49bea
perry523 <wendel.renato@gmail.com>
2023-12-15 09:46:05 -0300

    removendo vite-pwa

--- volumes/dinocash/vite.config.js
+++ volumes/dinocash/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
     plugins: [
-        VitePWA({
-            registerType: "autoUpdate",
-            devOptions: { injectRegister: false },
-            // filename: "sw.js",
-            strategies: "injectManifest",
-            
-        }),
         laravel({
             input: "resources/js/app.js",
             ssr: "resources/js/ssr.js",
