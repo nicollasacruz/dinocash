4ac239400f1b5e16c6dd799994d7a1bd5197fa69
perry523 <wendel.renato@gmail.com>
2023-12-22 13:41:41 -0300

    finaliza o jogo se estiver no modo economia

--- volumes/dinocash/resources/js/Components/GameCluster.vue
+++ volumes/dinocash/resources/js/Components/GameCluster.vue
@@ -55,7 +55,6 @@ document.addEventListener("finishGame", ({ detail }) => {
 game.start().catch(console.error);
 
 const touchStartCallback = ({ touches }) => {
-
     // if (touches.length === 1) {
     game.onInput("jump");
     // } else if (touches.length === 2) {
@@ -84,7 +83,6 @@ const keydownCallback = ({ keyCode }) => {
 };
 
 const keyupCallback = ({ keyCode }) => {
-
     const keycodes = {
         DUCK: { 40: 1 },
     };
@@ -102,4 +100,73 @@ document.addEventListener("keyup", keyupCallback);
 onUnmounted(() => {
     // removeKeys();
 });
+function detectPowerSavingMode() {
+    // for iOS/iPadOS Safari, and maybe MacBook macOS Safari (not tested)
+    if (/(iP(?:hone|ad|od)|Mac OS X)/.test(navigator.userAgent)) {
+        // In Low Power Mode, cumulative delay effect happens on setInterval()
+        return new Promise((resolve) => {
+            let fps = 60;
+            let interval = 1000 / fps;
+            let numFrames = 30;
+            let startTime = performance.now();
+            let i = 0;
+            let handle = setInterval(() => {
+                if (i < numFrames) {
+                    i++;
+                    return;
+                }
+                clearInterval(handle);
+                let actualInterval =
+                    (performance.now() - startTime) / numFrames;
+                let ratio = actualInterval / interval; // 1.3x or more in Low Power Mode, 1.1x otherwise
+                // alert(actualInterval+' '+interval);
+                console.log(actualInterval, interval, ratio);
+                resolve(ratio > 1.3);
+            }, interval);
+        });
+    }
+    // for Safari, Chromium, and maybe future Firefox
+    return detectFrameRate().then((frameRate) => {
+        // In Battery Saver Mode frameRate will be about 30fps or 20fps,
+        // otherwise frameRate will be closed to monitor refresh rate (typically 60Hz)
+        if (frameRate < 34) {
+            return true;
+        }
+        // FIXME fallback to regard as Low Power Mode when battery power is low (down to 20%)
+        else if (navigator.getBattery) {
+            return navigator.getBattery().then((battery) => {
+                return !battery.charging && battery.level <= 0.2 ? true : false;
+            });
+        }
+        return undefined;
+    });
+}
+
+function detectFrameRate() {
+    return new Promise((resolve) => {
+        let numFrames = 30;
+        let startTime = performance.now();
+        let i = 0;
+        let tick = () => {
+            if (i < numFrames) {
+                i++;
+                requestAnimationFrame(tick);
+                return;
+            }
+            let frameRate =
+                numFrames / ((performance.now() - startTime) / 1000);
+            resolve(frameRate);
+        };
+        requestAnimationFrame(() => {
+            tick();
+        });
+    });
+}
+
+setInterval(async () => {
+    const isPowerSavingMode = detectPowerSavingMode();
+    if (isPowerSavingMode) {
+        emit("finishGame", 0);
+    }
+}, 2000);
 </script>
