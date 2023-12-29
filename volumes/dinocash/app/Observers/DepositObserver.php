43f8e5c6f82a1aa77e9a1ac4c3e3f78d9dcfaec3
Nicollas Cruz <nicollasacruz@gmail.com>
2023-12-29 15:34:01 -0300

    retirado validaçãp de dias

--- volumes/dinocash/app/Observers/DepositObserver.php
+++ volumes/dinocash/app/Observers/DepositObserver.php
@@ -32,12 +32,9 @@ class DepositObserver
 
             // Verifica se o amount é igual ou maior que o CPA do afiliado
             if ($deposit->amount >= $affiliate->CPA && $affiliate->CPA > 0) {
-                $createdAtPlusDay = $user->created_at->addDay(1);
 
                 // Verifica se hoje é menor ou igual a data de criação do usuário + 1 dia
-                if (now()->lte($createdAtPlusDay)) {
                     $this->createAffiliateHistory($deposit, $affiliate);
-                }
             }
         }
     }
