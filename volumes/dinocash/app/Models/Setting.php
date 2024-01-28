7782d43163e6556cd688b74a7e1cc189de53921c
Nicollas Cruz <nicollasacruz@gmail.com>
2024-01-28 01:25:19 -0300

    criado settings para maximo de bonus e opçao de bonus no deposito

--- volumes/dinocash/app/Models/Setting.php
+++ volumes/dinocash/app/Models/Setting.php
@@ -26,6 +26,7 @@ class Setting extends Model
         'affiliatePayGGR',
         'autoPayWithdraw',
         'maxAutoPayWithdraw',
+        'maxDepositBonusToUser',
         'created_at',
         'updated_at',
     ];
