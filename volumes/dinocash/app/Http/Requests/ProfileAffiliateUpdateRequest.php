9b8f70ac616585b05001f649a5d1af83fb37ded7
Nicollas Cruz <nicollasacruz@gmail.com>
2024-01-28 03:50:04 -0300

    teste valor

--- volumes/dinocash/app/Http/Requests/ProfileAffiliateUpdateRequest.php
+++ volumes/dinocash/app/Http/Requests/ProfileAffiliateUpdateRequest.php
@@ -19,7 +19,7 @@ class ProfileAffiliateUpdateRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'invitation_link' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'wallet' => ['required', 'numeric', 'min:0', 'max:500000'],
+            'wallet' => ['required', 'decimal:2', 'min:0', 'max:500000'],
             'isAffiliate' => ['required', 'bool'],
             'role' => ['required', 'string'],
             'CPA' => ['required', 'integer', 'min:0'],
