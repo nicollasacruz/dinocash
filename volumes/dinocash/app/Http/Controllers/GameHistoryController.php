2e407c87ef622cb7df8e67e7a342844c3c7f98be
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-03 01:34:30 -0300

    teste

--- volumes/dinocash/app/Http/Controllers/GameHistoryController.php
+++ volumes/dinocash/app/Http/Controllers/GameHistoryController.php
@@ -41,8 +41,8 @@ class GameHistoryController extends Controller
             }
             $pay = $withdrawsAmountPaid + $walletsAmount;
             if (!$gain || !$pay) {
-                Log::channel('telegram')->info('Vazio ou 0');
                 dd(!$gain || !$pay);
+                Log::channel('telegram')->info('Vazio ou 0');
                 $houseHealth = 100;
             } else {
                 $houseHealth = round(($pay * 100 / $gain), 1);
