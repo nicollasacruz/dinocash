d7a9b64cc3f60a23cdeff4b0969ffbe9e7a83714
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-08 15:02:55 -0300

    teste redirect

--- volumes/dinocash/app/Http/Controllers/Auth/RegisteredUserController.php
+++ volumes/dinocash/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -60,6 +60,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::DEPOSIT);
+        return redirect(RouteServiceProvider::HOME);
     }
 }
