cb01f72f9184cd171529e7c52b8218ecd669b1af
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-13 00:16:46 +0000

    ajust user header

--- volumes/dinocash/resources/js/Components/UserHeader.vue
+++ volumes/dinocash/resources/js/Components/UserHeader.vue
@@ -1,71 +1,55 @@
 <template>
     <template v-if="logged">
         <div
-            class="bg-[#150822] flex justify-between items-center h-16 py-2 lg:h-24 lg:py-6 px-1 sm:px-2 lg:grid lg:grid-cols-12"
-        >
-            <div
-                v-if="logged"
-                class="flex flex-1 lg:justify-normal lg:col-start-2 xl:col-start-3 items-center"
-            >
-                <bars3-icon
-                    @click="emit('toggle')"
-                    class="w-6 min-w-[20px] sm:w-8 sm:h-8 cursor-pointer fill-white lg:hidden ml-1 lg:ml-3 mr-3"
-                />
+            class="bg-[#150822] flex justify-between items-center h-16 py-2 lg:h-24 lg:py-6 px-1 sm:px-2 lg:grid lg:grid-cols-12">
+            <div v-if="logged" class="flex flex-1 lg:justify-normal lg:col-start-2 xl:col-start-3 items-center">
+                <bars3-icon @click="emit('toggle')"
+                    class="w-6 min-w-[20px] sm:w-8 sm:h-8 cursor-pointer fill-white lg:hidden ml-1 lg:ml-3 mr-3" />
                 <img class="h-5 sm:h-10 lg:block" :src="DinoLogo" />
             </div>
-            <div
-                class="flex sm:flex-1 items-center justify-end gap-x-1 sm:gap-x-2 lg:col-end-11 xl:col-end-12 pr-3"
-            >
+            <div class="flex sm:flex-1 items-center justify-end gap-x-1 sm:gap-x-2 lg:col-end-11 xl:col-end-12 pr-3">
                 <div
-                    class="bg-verde relative lg:flex justify-center items-center rounded font-bold sm:text-md hidden h-9 text-roxo"
-                >
+                    class="bg-verde relative lg:flex justify-center items-center rounded font-bold sm:text-md hidden h-9 text-roxo">
                     <div class="w-32 text-center">
                         {{ userName }}
                     </div>
                 </div>
 
-                <Link class="font-bold ml-2 mr-1 lg:mr-0 lg:ml-0 text-xs lg:text-base rounded-md border-verde" :href="route('user.deposito')">
-                    <div>
-                        DEPOSITAR
-                    </div>
+                <Link class="font-bold ml-2 mr-1 lg:mr-0 lg:ml-0 text-xs lg:text-base rounded-md border-verde"
+                    :href="route('user.deposito')">
+                <div>
+                    DEPOSITAR
+                </div>
                 </Link>
 
-                <div
+                <div 
                     @pointerdown="bonusActive = false"
                     @pointerup="bonusActive = true"
                     @mouseover="bonusActive = false"
                     @mouseleave="bonusActive = true"
-                    class="text-sm sm:text-sm font-bold text-verde p-1 sm:px-3 rounded-full border-2 border-verde flex justify-center items-center"
-                >
-                {{ toBRL(money) }}
-                <WalletIcon
-                    class="w-4 sm:w-4 lg:mt-1 -mr-2 lg:-mr-5 fill-white"
-                />
+                    class="text-sm sm:text-sm font-bold text-verde p-1 px-2 sm:px-3 rounded-full border-2 border-verde flex justify-center items-center">
+                    
+                    {{ toBRL(money) }}
+
+                    <WalletIcon class="w-4 sm:w-4 lg:mt-1 -mr-2 lg:-mr-5 fill-white" />
                 </div>
                 <Link class="" :href="route('logout')" method="post">
-                    <img class="w-5 sm:w-7 mt-2 mr-2" :src="leave" />
+                <img class="w-5 sm:w-7 lg:w-14 mt-2 ml-2" :src="leave" />
                 </Link>
             </div>
         </div>
     </template>
-    <div
-        v-else
-        class="bg-roxo flex justify-center items-center h-20 py-6 px-1 sm:px-2 py-2"
-    >
+    <div v-else class="bg-roxo flex justify-center items-center h-20 py-6 px-1 sm:px-2 py-2">
         <div class="flex gap-x-3">
             <Link :href="route('login')">
-                <div
-                    class="user-button py-2 px-5 lg:px-10 lg:text-base lg:py-3 items-center flex"
-                >
-                    Login
-                </div>
+            <div class="user-button py-2 px-5 lg:px-10 lg:text-base lg:py-3 items-center flex">
+                Login
+            </div>
             </Link>
             <Link :href="route('register')">
-                <div
-                    class="user-button py-2 px-5 lg:px-10 lg:text-base lg:py-3 items-center flex"
-                >
-                    Registrar
-                </div>
+            <div class="user-button py-2 px-5 lg:px-10 lg:text-base lg:py-3 items-center flex">
+                Registrar
+            </div>
             </Link>
         </div>
     </div>
