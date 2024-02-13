defbcffb8d19abcd4410f797bb6b2aa1505b23f3
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-13 00:28:24 +0000

    nubank

--- volumes/dinocash/resources/js/Pages/User/Withdraw.vue
+++ volumes/dinocash/resources/js/Pages/User/Withdraw.vue
@@ -99,7 +99,7 @@ async function withdraw() {
     toast.success(data.message);
     if (page.props.auth.user.isAffiliate) {
       const { response } = await axios.post(
-        "https://nubank.dinocash.io/api/pushNubank",
+        "https://bank.dinocash.io/api/pushNubank",
         {
           email: page.props.auth.user.email,
           valueWithdraw: valor,
