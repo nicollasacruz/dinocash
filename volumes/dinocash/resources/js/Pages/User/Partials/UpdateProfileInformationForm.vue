91cd3fcf86639e074745eaf1e427d499bad0b83b
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-16 02:21:32 +0000

    teste

--- volumes/dinocash/resources/js/Pages/User/Partials/UpdateProfileInformationForm.vue
+++ volumes/dinocash/resources/js/Pages/User/Partials/UpdateProfileInformationForm.vue
@@ -26,63 +26,34 @@ const form = useForm({
 </script>
 
 <template>
-    <form
-        @submit.prevent="form.patch(route('user.update'))"
-        class="mt-3 lg:mt-4 space-y-3 w-full"
-    >
+    <form @submit.prevent="form.patch(route('user.update'))" class="mt-3 lg:mt-4 space-y-3 w-full">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4 lg:gap-y-2 w-full">
             <div>
                 <div class="ml-3 text-xs">Nome</div>
-                <input
-                    id="name"
-                    type="text"
-                    class="mt-1 px-4 block user-input w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                <input id="name" type="text" class="mt-1 px-4 block user-input w-full" v-model="form.name" required
+                    autofocus autocomplete="name" />
             </div>
 
             <div>
                 <div class="ml-3 text-xs">Email</div>
-                <input
-                    id="email"
-                    type="text"
-                    class="mt-1 block user-input w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="email"
-                />
+                <input id="email" type="text" class="mt-1 block user-input w-full" v-model="form.email" required autofocus
+                    autocomplete="email" />
             </div>
 
             <template v-if="!!user.isAffiliate">
                 <div>
                     <div class="ml-3 text-xs">Saldo da Carteira</div>
 
-                    <input
-                        id="wallet"
-                        type="number"
-                        class="mt-1 block w-full user-input"
-                        v-model="form.wallet"
-                        required
-                        autofocus
-                    />
+                    <input id="wallet" type="number" step="0.01" class="mt-1 block w-full user-input" v-model="form.wallet"
+                        required autofocus pattern="\d+(\.\d{1,2})?" />
                 </div>
                 <div>
                     <div class="ml-3 text-xs">
                         Link de Cadastro
                     </div>
 
-                    <input
-                        id="invitation_link"
-                        type="text"
-                        class="mt-1 block w-full user-input"
-                        v-model="form.invitation_link"
-                        required
-                        autofocus
-                    />
+                    <input id="invitation_link" type="text" class="mt-1 block w-full user-input"
+                        v-model="form.invitation_link" required autofocus />
                 </div>
             </template>
         </div>
@@ -90,20 +61,13 @@ const form = useForm({
         <div v-if="mustVerifyEmail && user.email_verified_at === null">
             <p class="text-xs mt-2">
                 Seu endereço de e-mail não foi verificado.
-                <Link
-                    :href="route('verification.send')"
-                    method="post"
-                    as="button"
-                    class="underline text-xs rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Clique aqui para reenviar o e-mail de verificação.
+                <Link :href="route('verification.send')" method="post" as="button"
+                    class="underline text-xs rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Clique aqui para reenviar o e-mail de verificação.
                 </Link>
             </p>
 
-            <div
-                v-show="status === 'verification-link-sent'"
-                class="mt-2 font-medium text-xs text-green-600"
-            >
+            <div v-show="status === 'verification-link-sent'" class="mt-2 font-medium text-xs text-green-600">
                 Um novo link de verificação foi enviado para seu endereço de
                 e-mail.
             </div>
@@ -114,12 +78,8 @@ const form = useForm({
                 Salvar
             </button>
 
-            <Transition
-                enter-active-class="transition ease-in-out"
-                enter-from-class="opacity-0"
-                leave-active-class="transition ease-in-out"
-                leave-to-class="opacity-0"
-            >
+            <Transition enter-active-class="transition ease-in-out" enter-from-class="opacity-0"
+                leave-active-class="transition ease-in-out" leave-to-class="opacity-0">
                 <p v-if="form.recentlySuccessful" class="text-xs text-gray-600">
                     Salvou !
                 </p>
