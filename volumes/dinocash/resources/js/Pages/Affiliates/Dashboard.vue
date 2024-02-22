44f83076ebb8a79d7eed459dc3935224c3378767
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-22 13:49:03 +0000

    testar limpar bonus

--- volumes/dinocash/resources/js/Pages/Affiliates/Dashboard.vue
+++ volumes/dinocash/resources/js/Pages/Affiliates/Dashboard.vue
@@ -158,7 +158,7 @@ async function clearBonus() {
     await axios.post(route("afiliado.bonus.clear"));
     toast.success("Bônus apagado com sucesso");
   } catch (error) {
-    toast.error(error.response.data.message);
+    toast.error(error);
   }
 }
 
