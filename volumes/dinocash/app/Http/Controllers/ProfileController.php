e6268983499be8494927bd0afdd30ef8a50b1be2
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-29 12:59:09 +0000

    ajuste referralsDepositsCounter

--- volumes/dinocash/app/Http/Controllers/ProfileController.php
+++ volumes/dinocash/app/Http/Controllers/ProfileController.php
@@ -33,6 +33,7 @@ class ProfileController extends Controller {
         if($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
+        unset($request->user()->referralsDepositsCounter);
 
         $request->user()->invitation_link = $request->user()->invitation_link;
         $request->user()->save();
