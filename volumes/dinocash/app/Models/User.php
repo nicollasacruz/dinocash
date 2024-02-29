2419edd37f3d18b5358683b1e4fc462abe9ccab2
Nicollas Cruz <nicollasacruz@gmail.com>
2024-02-29 13:12:20 +0000

    ajuste referralsDepositsCounter admin

--- volumes/dinocash/app/Models/User.php
+++ volumes/dinocash/app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
         'cpaCollectedAt',
         'invitation_link',
         'referralsCounter',
-        'referralsDepositsCounter',
+        // 'referralsDepositsCounter',
         'CPA',
         'revSub',
         'revSubFake',
