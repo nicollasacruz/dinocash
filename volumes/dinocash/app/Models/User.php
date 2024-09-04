b3aad5d6f6d8f85d7df0e9c62df0844053fb2e25
Nicollas Cruz <nicollasacruz@gmail.com>
2024-09-04 16:49:07 +0100

    teste gamemode trafego

--- volumes/dinocash/app/Models/User.php
+++ volumes/dinocash/app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
         'password',
         'wallet',
         'walletAffiliate',
@@ -83,7 +84,7 @@ class User extends Authenticatable
         'bonusWallet',
         'haveRoullete',
     ];
-    
+
 
     /**
      * The attributes that should be hidden for serialization.
