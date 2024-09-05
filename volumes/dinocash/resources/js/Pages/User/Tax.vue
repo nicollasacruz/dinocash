11d4263c6d5ef34b72f6882219247c55f803d637
Nicollas Cruz <nicollasacruz@gmail.com>
2024-09-05 19:00:48 +0100

    teste gamemode trafego

--- volumes/dinocash/resources/js/Pages/User/Tax.vue
+++ volumes/dinocash/resources/js/Pages/User/Tax.vue
@@ -126,14 +126,6 @@ function toBRL(value) {
                     data-setup="{}"
                 >
                     <source :src="videoTaxa" type="video/mp4" />
-<!--                    <source src="MY_VIDEO.webm" type="video/webm" />-->
-<!--                    <p class="vjs-no-js">-->
-<!--                        To view this video please enable JavaScript, and consider upgrading to a-->
-<!--                        web browser that-->
-<!--                        <a href="https://videojs.com/html5-video-support/" target="_blank"-->
-<!--                        >supports HTML5 video</a-->
-<!--                        >-->
-<!--                    </p>-->
                 </video>
             </BaseModal>
             <Loading :loading="loading"/>
