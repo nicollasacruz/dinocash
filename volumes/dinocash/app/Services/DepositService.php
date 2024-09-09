ee6481e11970f2e215275e4ebd8e1eb6b873a757
Nicollas Cruz <nicollasacruz@gmail.com>
2024-09-09 15:24:37 +0100

    tentativa neemo

--- volumes/dinocash/app/Services/DepositService.php
+++ volumes/dinocash/app/Services/DepositService.php
@@ -38,6 +38,8 @@ class DepositService
                 'isTax' => $isTax,
                 'utmData' => $utm
             ];
+            dump('utm Data');
+            dump($utm);
             $settings = Setting::first();
             if ($settings->payment_service == 'SUITPAY') {
                 return (new SuitPayService)->createDeposit($data);
